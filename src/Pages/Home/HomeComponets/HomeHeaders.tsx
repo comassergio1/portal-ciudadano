701c87226d0da2ed639f07571226936b65653bbf
julianov <julianov403@gmail.com>
2022-05-14 14:34:34 -0300

    arreglo dependencias bootstrap

--- src/Pages/Home/HomeComponets/HomeHeaders.tsx
+++ src/Pages/Home/HomeComponets/HomeHeaders.tsx
@@ -28,17 +28,6 @@ return (
         </Container>
     </Navbar>
 
-    <Navbar bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="#home">Home</Nav.Link>
-      <Nav.Link href="#features">Features</Nav.Link>
-      <Nav.Link href="#pricing">Pricing</Nav.Link>
-    </Nav>
-    </Container>
-  </Navbar>    
-
     </>
 )
 
