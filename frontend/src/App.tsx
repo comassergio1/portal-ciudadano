fa8a9860bd0684458776c618d2d80001d25b26a5
julianov <julianov403@gmail.com>
2022-09-04 11:57:44 -0300

    dashboard-ciudadano

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@
 import './Styles/App.css';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { DefaultLayout } from './Components/Layout/DefaultLayout';
-import { HomePage } from './Pages/HomePage';
+import { HomeInitPage } from './Pages/HomePage';
 import { TramitesPage } from './Pages/Tramites/index';
 import { ErrorPage } from './Pages/ErrorPage';
 import { ResetPassword } from './Pages/Auth/ResetPassword';
@@ -16,17 +16,18 @@ function App() {
     <BrowserRouter>
         <Routes>
           <Route path="/" element={<DefaultLayout />}>
-            <Route index element={<HomePage />} />
+            <Route index element={<HomeInitPage />} />
             <Route path="Ingresar" element={<LoginPage />} />
             <Route path="Registro" element={<RegisterPage />} />
             <Route path="RestaurarPassword" element={<ResetPassword />} />
-            <Route path="Tramites"
+            <Route path="servicios"
               element={
                 <PrivateRoute>
                   <TramitesPage />
                 </PrivateRoute>
               }
             />
+          
             <Route path="*" element={<ErrorPage />} />
           </Route>
         </Routes>
